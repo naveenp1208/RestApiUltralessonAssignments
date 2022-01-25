ee6d8e9624da097721c89a10e564cb47a518014f
naveenparvatham <naveenp@testvagrant.com>
2022-01-25 10:26:16 +0530

    initial commit

---
+++ src/test/java/Sampletest.java
@@ -0,0 +1,9 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Sampletest {
+    @Test
+    public void shouldTestSomething(){
+        Assert.assertEquals(2,2);
+    }
+}
