22a433384b1f9f9e3dd495cba1fb78547476ebcf
naveenparvatham <naveenp@testvagrant.com>
2022-01-25 12:05:43 +0530

    first Api test

--- src/test/java/Sampletest.java
+++ src/test/java/Sampletest.java
@@ -1,9 +1,22 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class Sampletest {
     @Test
-    public void shouldTestSomething(){
-        Assert.assertEquals(2,2);
+    public void shouldGetAllUsers(){
+
+        //Arrange
+        //Act
+        //Assert
+        given()
+                .when()
+                     .get("https://gorest.co.in/public/v1/users")
+                .then()
+                     .statusCode(200)
+                     .log().body();
+
+
     }
 }
