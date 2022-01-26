31d177f83d78d846b44027d30049afab0a5391b0
naveenparvatham <naveenp@testvagrant.com>
2022-01-26 11:29:53 +0530

    Added a test to create new user

--- src/test/java/Sampletest.java
+++ src/test/java/Sampletest.java
@@ -1,3 +1,4 @@
+import io.restassured.http.ContentType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -19,4 +20,31 @@ public class Sampletest {
 
 
     }
+
+
+    @Test
+    public void shouldCreateUser() {
+        // 1. Arrange
+        // 2. Act
+        // 3. Assert
+
+        given()
+                        .accept(ContentType.JSON)
+                        .contentType(ContentType.JSON)
+                        .header("Authorization","Bearer ed50b50c3f76a6e0dfbf0f4e87f0ca7f46fda8cc0f68632d2d86e1bf369949ec")
+                        .body("{\n" +
+                                "  \"name\": \"Tenali Ramakrishna\",\n" +
+                                "  \"gender\": \"male\",\n" +
+                                "  \"email\": \"tenali.ramakrishna32@gmail.com\",\n" +
+                                "  \"status\": \"active\"\n" +
+                                "}")
+                .when()
+                     .post("https://gorest.co.in/public/v1/users")
+                .then()
+                     .log().body()
+                     .statusCode(201);
+
+
+    }
+
 }
